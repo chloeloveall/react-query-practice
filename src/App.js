f91958e6450e2fd880ea7b5c8e612d363a40ebff
Chloe Loveall <cloveall@lightspeedsystems.com>
2021-08-22 10:57:44 -0700

    Add react-query-devtools with initial open set to false

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import People from './components/People';
 import Planets from './components/Planets';
+import { ReactQueryDevtools } from 'react-query-devtools';
+// import { QueryClient, QueryClientProvider } from 'react-query'
+
+// const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState('planets');
 
+  // <QueryClientProvider client={queryClient}>
   return (
-    <div className='App'>
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className='content'>{page === 'planets' ? <Planets /> : <People />}</div>
-    </div>
+    <>
+      <div className='App'>
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className='content'>{page === 'planets' ? <Planets /> : <People />}</div>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
