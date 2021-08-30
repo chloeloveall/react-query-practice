563abbb35888d84d40a1d752d600cdead26849a9
Chloe Loveall <cloveall@lightspeedsystems.com>
2021-08-29 18:40:29 -0700

    Update query method to useQuery with keepPreviousData set to true

--- src/components/People.js
+++ src/components/People.js
@@ -1,45 +1,48 @@
 import React, { useState } from 'react';
-import { usePaginatedQuery } from 'react-query';
+import { useQuery } from 'react-query';
 import Person from './Person';
 
 function People() {
   const [page, setPage] = useState(1);
-  const fetchPeople = async (key, page) => {
-    const res = await fetch(`http://swapi.dev/api/people/?page=${page}`);
-    return res.json();
-  };
 
-  // first argument is a key for the query
-  // second argument is an async function to fetch the data
-  const { resolvedData, latestData, status } = usePaginatedQuery(['people', page], fetchPeople);
+  const fetchPeople = (page = 1) => fetch(`http://swapi.dev/api/people/?page=${page}`).then((res) => res.json());
+
+  const { isLoading, isError, error, data, isPreviousData } = useQuery(['people', page], () => fetchPeople(page), {
+    keepPreviousData: true,
+  });
 
   return (
     <div>
       <h2>People</h2>
-      {status === 'loading' && <div>Loading data</div>}
-      {status === 'error' && <div>Error fetching data</div>}
-      {status === 'success' && (
-        <>
-          <div>
-            {resolvedData.results.map((person) => (
-              <Person key={person.name} person={person} />
-            ))}
-          </div>
-
-          <div className='paginationDiv'>
-            <button onClick={() => setPage((old) => Math.max(old - 1, 1))} disabled={page === 1}>
-              previous page
-            </button>
-            <span>{page}</span>
-            <button
-              onClick={() => setPage((old) => (!latestData || !latestData.next ? old : old + 1))}
-              disabled={!latestData || !latestData.next}
-            >
-              next page
-            </button>
-          </div>
-        </>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <div>
+          {data.results.map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
+        </div>
       )}
+
+      <div className='paginationDiv'>
+        <button onClick={() => setPage((old) => Math.max(old - 1, 1))} disabled={page === 1}>
+          previous page
+        </button>
+        <span>{page}</span>
+        <button
+          onClick={() => {
+            if (!isPreviousData && data.next) {
+              setPage((old) => old + 1);
+            }
+          }}
+          // Disable the Next Page button until we know a next page is available
+          disabled={isPreviousData || !data?.next}
+        >
+          Next Page
+        </button>
+      </div>
     </div>
   );
 }
